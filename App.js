d01b6ddd66e48411a157dc57f4db0a7c3e6b23aa
Sampo Jaakkola <sampo.jaakkola@myy.haaga-helia.fi>
2024-03-27 07:40:20 +0200

    Bring text to speech app to GitHub

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, TextInput, View, Button } from "react-native";
+import * as Speech from "expo-speech";
 
 export default function App() {
+  const [text, setText] = useState("");
+
+  const toSpeech = () => {
+    Speech.speak(text);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput style={styles.input} onChangeText={(text) => setText(text)} />
+      <Button title="Press to hear text" onPress={toSpeech} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    width: 200,
+    borderColor: "grey",
+    borderWidth: 1,
   },
 });
